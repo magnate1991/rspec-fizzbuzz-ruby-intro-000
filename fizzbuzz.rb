105ea7c93fb9176f690dff11fe6b90e5a9160761
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-08 11:44:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 and int % 5 == 0
-    return "FizzBuzz"
-  elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 3 == 0
-    return  "Fizz"
+require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
 
-  end
 
-end
+describe "fizzbuzz" do	describe "fizzbuzz" do
+  it 'returns "Fizz" when the number is divisible by 3' do	  it 'returns "Fizz" when the number is divisible by 3' do
